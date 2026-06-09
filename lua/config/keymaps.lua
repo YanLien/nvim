2af520c226fdc65b0075d1ab14c6685de7666866
YanLien <YJQ980314@outlook.com>
2026-06-09 11:24:13 +0800

    feat(nvim): add file path copy keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,17 @@ map("n", "zl", "<cmd>vertical resize +5<cr>", { desc = "→ Increase width" })
 map("n", "zj", "<cmd>resize -3<cr>", { desc = "↓ Decrease height" })
 map("n", "zk", "<cmd>resize +3<cr>", { desc = "↑ Increase height" })
 
+-- 复制当前文件绝对路径
+map("n", "<leader>yP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied path: " .. path)
+end, { desc = "Copy absolute file path" })
+
+-- 复制当前文件相对路径
+map("n", "<leader>yp", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied relative path: " .. path)
+end, { desc = "Copy relative file path" })
+
